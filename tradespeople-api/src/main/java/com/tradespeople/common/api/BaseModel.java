7e528fe3f75ab53f4de3adf8954cb84dc69c820a
cetinimre <cetinimre@vm-0.cetinimre.koding.kd.io>
2013-08-15 22:11:49 +0000

    First Commit from Kodind Cloud Platform

--- tradespeople-api/src/main/java/com/tradespeople/common/api/BaseModel.java
+++ tradespeople-api/src/main/java/com/tradespeople/common/api/BaseModel.java
@@ -1,5 +1,41 @@
 package com.tradespeople.common.api;
 
 public class BaseModel {
+    
+    private Long Id;
+    private Date createdDate;
+	
+	public BaseModel(Long id,Date createdDate){
+		this.Id=id;
+		this.createdDate=createdDate;
+	}
+	
+	public BaseModel(Long id) {
+		this(id,new Date());
+	}
+	
+	public BaseModel(){
+		createdDate=new Date();
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	
+	public boolean isPersisted(){
+		return Id!=null;
+	}
 
 }
